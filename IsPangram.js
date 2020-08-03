53f62a13348d81d6b2d38bb3759f4d43ce471aed
Nicholas Amemazior <65061618+Nickgld@users.noreply.github.com>
2020-08-03 21:43:40 +0000

    Update IsPangram.js

--- IsPangram.js
+++ IsPangram.js
@@ -1,3 +1,5 @@
+//This method works by comparing the string with each letter from the alphabet 
+
 function isPangram(string){
   var alpha = "abcdefghijklmnopqrstuvwxyz";
   //expression for spaces
@@ -15,3 +17,33 @@ function isPangram(string){
   }
   return true;
 }
+
+
+
+//this method works by using objects
+function isPangram(string){
+  var obj = {};
+  var sum = 0;
+  var newString = string.replace(/\s+/g, "").toLowerCase();
+  
+  //if the letter is found in obj, continue; else add it to obj with a value of 1
+  for(var i = 0; i < newString.length; i++){
+    if(obj[newString[i]]){
+    } else {
+      obj[newString[i]] = 1;
+    }
+  }
+  
+  //add all the values of obj
+  for(var j in obj){
+    sum += obj[j]
+  }
+  
+  //checks if all 26 letters are present, return true; else return false
+  if(sum == 26){
+    return true;
+  } else {
+    return false;
+  }
+}
+
