4c5851714768ac737ccc539c20896c7657535cfb
Nicholas Amemazior <65061618+Goldac77@users.noreply.github.com>
2021-03-23 12:34:11 +0000

    Update beeramid.js

--- beeramid.js
+++ beeramid.js
@@ -4,7 +4,7 @@ var beeramid = function(bonus, price) {
   var x = 1;
   var y = 0;
   var count = 0;
-  while (y < beer) { //sum the square of consecutive natural numbers until value is greater than "beer"
+  while (y < beer) { //sum the square of consecutive natural numbers while value is less than "beer"
     x++;
     y += (x**2)
     count++          //counting each loop to represent beeramid level
