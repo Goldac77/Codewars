34e3d3c0c76aaaddb40b824fa06d03f2ad13070d
Nicholas Amemazior <65061618+Goldac77@users.noreply.github.com>
2022-05-08 04:25:52 +0000

    Create Vowel remover.py

---
+++ Vowel remover.py
@@ -0,0 +1,10 @@
+def shortcut( s ):
+    vowels = ["a", "e", "i", "o", "u"]
+    a = ""
+    for i in s:
+        if(i in vowels):
+            pass
+        else:
+            a += i
+    
+    return a
